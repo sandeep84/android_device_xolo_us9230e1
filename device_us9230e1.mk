a10d3942599e979985e0aeaaf5b6d970416e7c59
Ankur Raikwar <ankur5technical@gmail.com>
2013-08-10 20:58:35 +0530

    Update and rename device_t1000.mk to device_us9230e1.mk

--- device_us9230e1.mk
+++ device_us9230e1.mk
@@ -3,11 +3,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-$(call inherit-product-if-exists, vendor/xolo/t1000/t1000-vendor.mk)
+$(call inherit-product-if-exists, vendor/xolo/us9230e1/t1000-vendor.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/xolo/t1000/overlay
+DEVICE_PACKAGE_OVERLAYS += device/xolo/us9230e1/overlay
 
-LOCAL_PATH := device/xolo/t1000
+LOCAL_PATH := device/xolo/us9230e1
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
 else
@@ -20,5 +20,5 @@ PRODUCT_COPY_FILES += \
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-PRODUCT_NAME := full_t1000
-PRODUCT_DEVICE := t1000
+PRODUCT_NAME := full_us9230e1
+PRODUCT_DEVICE := us9230e1
