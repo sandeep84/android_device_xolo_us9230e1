853586c046f1e76adb8e8d723dcf8b96cf0b3d38
Ankur Raikwar <ankur5technical@gmail.com>
2013-08-10 20:54:35 +0530

    Update common.mk

--- common.mk
+++ common.mk
@@ -1,9 +1,9 @@
 ## Recovery
 PRODUCT_COPY_FILES += \
-    device/xolo/t1000/recovery/fstab:recovery/root/etc/fstab
+    device/xolo/us9230e1/recovery/fstab:recovery/root/etc/fstab
 
 ## Ramdisk
 PRODUCT_COPY_FILES += \
-    device/xolo/t1000/ramdisk/init.us9230e1.rc:root/init.us9230e1.rc \
-    device/xolo/t1000/ramdisk/ueventd.us9230e1.rc:root/ueventd.us9230e1.rc \
-    device/xolo/t1000/ramdisk/init.us9230e1.usb.rc:root/init.us9230e1.usb.rc 
+    device/xolo/us9230e1/ramdisk/init.us9230e1.rc:root/init.us9230e1.rc \
+    device/xolo/us9230e1/ramdisk/ueventd.us9230e1.rc:root/ueventd.us9230e1.rc \
+    device/xolo/us9230e1/ramdisk/init.us9230e1.usb.rc:root/init.us9230e1.usb.rc 
